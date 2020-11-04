521144680ddcefe8a0d1e76da8d8db92890e833d
Brandon Weaver <a7xncob@gmail.com>
2020-11-04 17:15:52 -0500

    Add task model, view, and view-model.

---
+++ TimeTrackerUI/Models/TaskModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTrackerUI.Models
+{
+    class TaskModel
+    {
+        public string Type { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public TaskModel(string type)
+        {
+            this.Type = type;
+        }
+
+        public void Start()
+        {
+            this.StartTime = DateTime.Now;
+        }
+
+        public void End()
+        {
+            this.EndTime = DateTime.Now;
+        }
+    }
+}
---
+++ TimeTrackerUI/ViewModels/TaskViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TimeTrackerUI.Models;
+
+namespace TimeTrackerUI.ViewModels
+{
+    class TaskViewModel
+    {
+        public ObservableCollection<TaskModel> TaskModels { get; set; }
+
+        public TaskViewModel()
+        {
+            this.TaskModels = new ObservableCollection<TaskModel>()
+            {
+                new TaskModel("Example")
+            };
+            this.TaskModels[0].Start();
+            this.TaskModels[0].End();
+        }
+    }
+}
