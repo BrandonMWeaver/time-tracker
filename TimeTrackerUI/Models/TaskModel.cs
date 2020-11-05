a8412e178b78ec35e14bf5698266841955e361fb
Brandon Weaver <a7xncob@gmail.com>
2020-11-05 16:19:34 -0500

    Restructure task model, and add duration to task model and view.

--- TimeTrackerUI/Models/TaskModel.cs
+++ TimeTrackerUI/Models/TaskModel.cs
@@ -8,8 +8,25 @@ namespace TimeTrackerUI.Models
     {
         public string Type { get; set; }
 
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public string StartTimeString
+        {
+            get { return this.StartTime.ToString("h:mm tt"); }
+        }
+
+        public string EndTimeString
+        {
+            get { return this.EndTime.ToString("h:mm tt"); }
+        }
+
+        public string DurationString
+        {
+            get { return this.Duration.ToString(@"h\:mm"); }
+        }
+
+        private DateTime StartTime { get; set; }
+        private DateTime EndTime { get; set; }
+
+        private TimeSpan Duration { get; set; }
 
         public TaskModel(string type)
         {
@@ -19,11 +36,20 @@ namespace TimeTrackerUI.Models
         public void Start()
         {
             this.StartTime = DateTime.Now;
+            if (this.EndTime != null)
+                this.SetDuration();
         }
 
         public void End()
         {
             this.EndTime = DateTime.Now;
+            if (this.StartTime != null)
+                this.SetDuration();
+        }
+
+        private void SetDuration()
+        {
+            this.Duration = this.EndTime - this.StartTime;
         }
     }
 }
