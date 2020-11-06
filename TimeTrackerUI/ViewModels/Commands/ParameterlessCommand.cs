ad4bc8804f2b887979218680f9b68e48724df70f
Brandon Weaver <a7xncob@gmail.com>
2020-11-06 14:26:22 -0500

    Add basic command system.

---
+++ TimeTrackerUI/ViewModels/Commands/ParameterlessCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeTrackerUI.ViewModels.Commands.Base;
+
+namespace TimeTrackerUI.ViewModels.Commands
+{
+    class ParameterlessCommand : CommandBase
+    {
+        private readonly Action _action;
+
+        public ParameterlessCommand(Action action) => this._action = action;
+
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => this._action.Invoke();
+    }
+}
