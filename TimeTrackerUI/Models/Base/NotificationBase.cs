81ee37081719743d5274b5491ae13c90f3441543
Brandon Weaver <a7xncob@gmail.com>
2020-11-06 13:22:17 -0500

    Add notification base.

---
+++ TimeTrackerUI/Models/Base/NotificationBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TimeTrackerUI.Models.Base
+{
+    class NotificationBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
